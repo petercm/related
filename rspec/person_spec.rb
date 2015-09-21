a7229dad78a152b317c77eda930c1868131f504c
John Brosnan <jbrosnan@constantcontact.com>
2015-09-21 13:16:42 -0400

    Tests

--- rspec/person_spec.rb
+++ rspec/person_spec.rb
@@ -15,11 +15,53 @@ describe Person do
   end
 
   describe '#is_related_to?' do
-    it 'detects child relationships'
-    it 'detects sibling relationships'
-    it 'detects uncle'
-    it 'detects grandparent'
-    it 'detects non-related persons'
+    it 'detects child relationships' do
+      parent = Person.new 'Dad', nil, nil
+      child = Person.new 'Son', nil, parent
+
+      expect(child.is_related_to?(parent)).to be_truthy
+      expect(parent.is_related_to?(child)).to be_truthy
+    end
+
+    it 'detects sibling relationships' do
+      parent = Person.new 'Dad', nil, nil
+      child = Person.new 'Son', nil, parent
+      child2 = Person.new 'Son2', nil, parent
+      expect(child.is_related_to?(child2)).to be_truthy
+      expect(child2.is_related_to?(child)).to be_truthy
+    end
+
+    it 'detects uncle' do
+      grandparent = Person.new 'Gramps', nil, nil
+      parent = Person.new 'Dad', nil, grandparent
+      child = Person.new 'Son', nil, parent
+      uncle = Person.new 'Joe', nil, grandparent
+
+      expect(child.is_related_to?(uncle)).to be_truthy
+      expect(uncle.is_related_to?(child)).to be_truthy
+    end
+
+    it 'detects grandparent' do
+      grandparent = Person.new 'Gramps', nil, nil
+      parent = Person.new 'Dad', nil, grandparent
+      child = Person.new 'Son', nil, parent
+
+      expect(child.is_related_to?(grandparent)).to be_truthy
+      expect(grandparent.is_related_to?(child)).to be_truthy
+    end
+
+    it 'detects non-related persons' do
+      grandparent = Person.new 'Gramps', nil, nil
+      parent = Person.new 'Dad', nil, grandparent
+      child = Person.new 'Son', nil, parent
+
+      parent2 = Person.new 'Dad', nil, nil
+      child2 = Person.new 'Son', nil, parent2
+
+      expect(child.is_related_to?(child2)).to be_falsy
+      expect(child2.is_related_to?(child)).to be_falsy
+    end
+
   end
 
 end
