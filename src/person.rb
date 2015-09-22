9a1d99bcd63ba7bec50ddaa6187532f426e81465
Peter C Miller <petercm@gmail.com>
2015-09-22 09:52:11 -0400

    Traverse the family tree to determine blood relation

--- src/person.rb
+++ src/person.rb
@@ -8,7 +8,16 @@ class Person
   end
 
   def is_related_to?(person)
-
+    !person.nil? && person != self && (
+      person == @father ||
+      person == @mother ||
+      self == person.father ||
+      self == person.mother ||
+      is_related_to?(person.father) ||
+      is_related_to?(person.mother) ||
+      (!@father.nil? && @father.is_related_to?(person)) ||
+      (!@mother.nil? && @mother.is_related_to?(person))
+    )
   end
 
 end
